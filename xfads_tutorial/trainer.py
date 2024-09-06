419341487156c83fba6585c513fceea9b267380b
Mahmoud Elmakki <MahmoudSMahdi7@gmail.com>
2024-09-06 09:20:00 +0100

    Updating trainer.py to match the current form of the notebook

--- xfads_tutorial/trainer.py
+++ xfads_tutorial/trainer.py
@@ -4,8 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#from api_neurotask import *
-
 import random
 
 from matplotlib import cm
@@ -40,33 +38,31 @@ from xfads.smoothers.nonlinear_smoother_causal import NonlinearFilter, LowRankNo
 import warnings
 warnings.filterwarnings("ignore")
 
-import os
-os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
-os.environ['CUDA_HOME'] = 'C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v12.1'
-
-torch.cuda.empty_cache()
+if torch.cuda.is_available():
+    import os
+    # To avoid GPU Memory Fragmentation
+    os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
 
 
 
 def main():
     
     """config"""
-
     cfg = {
         # --- graphical model --- #
         'n_latents': 40,
         'n_latents_read': 35,
-
+    
         'rank_local': 15,
         'rank_backward': 5,
-
+    
         'n_hidden_dynamics': 128,
-
+    
         # --- inference network --- #
         'n_samples': 25,
         'n_hidden_local': 256,
         'n_hidden_backward': 128,
-
+    
         # --- hyperparameters --- #
         'use_cd': False,
         'p_mask_a': 0.0,
@@ -75,46 +71,39 @@ def main():
         'p_mask_y_in': 0.0,
         'p_local_dropout': 0.4,
         'p_backward_dropout': 0.0,
-
+    
         # --- training --- #
-        'device': 'cuda',
+        'device': 'cpu',
         'data_device': 'cpu',
-
+     
         'lr': 1e-3,
         'n_epochs': 1000,
-        'batch_sz': 512,
-
+        'batch_sz': 32,
+        'minibatch_sz': 8,
+        'use_minibatching': False,
+    
         # --- misc --- #
         'bin_sz': 5e-3,
         'bin_sz_ms': 5,
-
-        'n_bins_bhv': 100,
-
-        'go_cue': 152,
-
+    
         'seed': 1236,
         'default_dtype': torch.float32,
-
+        
         # --- ray --- #
         'n_ray_samples': 10,
     }
-
-
+    
     class Cfg(dict):
         def __getattr__(self, attr):
             if attr in self:
                 return self[attr]
             else:
                 raise AttributeError(f"'DictAsAttributes' object has no attribute '{attr}'")
-
-
+    
     cfg = Cfg(cfg)
-
-    torch.cuda.empty_cache()
-
+    
     lightning.seed_everything(cfg.seed, workers=True)
     torch.set_default_dtype(torch.float32)
-
 
     """"Loading data"""
 
@@ -123,44 +112,47 @@ def main():
     val_data = torch.load(data_path.format(split='valid', bin_size_ms=cfg.bin_sz_ms))
     test_data = torch.load(data_path.format(split='test', bin_size_ms=cfg.bin_sz_ms))
 
+    # obs: observations
     y_train_obs = train_data['y_obs'].type(torch.float32).to(cfg.data_device)
     y_valid_obs = val_data['y_obs'].type(torch.float32).to(cfg.data_device)
     y_test_obs = test_data['y_obs'].type(torch.float32).to(cfg.data_device)
 
-    label_train = train_data['label'].type(torch.float32).to(cfg.data_device)[:, :, [2, 3]]
-    label_valid = val_data['label'].type(torch.float32).to(cfg.data_device)[:, :, [2, 3]]
-    label_test = test_data['label'].type(torch.float32).to(cfg.data_device)[:, :, [2, 3]]
+    # l: label
+    labels = ['cursor_vel_x', 'cursor_vel_y']
+    l_train = torch.tensor(np.array([train_data[l] for l in labels])).permute(1, 2, 0).type(torch.float32).to(cfg.data_device)
+    l_valid = torch.tensor(np.array([val_data[l] for l in labels])).permute(1, 2, 0).type(torch.float32).to(cfg.data_device)
+    l_test = torch.tensor(np.array([test_data[l] for l in labels])).permute(1, 2, 0).type(torch.float32).to(cfg.data_device)
 
-    train_moveonset = train_data['move_onset'].type(torch.float32).to(cfg.data_device)
-    valid_moveonset = val_data['move_onset'].type(torch.float32).to(cfg.data_device)
-    test_moveonset = test_data['move_onset'].type(torch.float32).to(cfg.data_device)
+    y_train_dataset = torch.utils.data.TensorDataset(y_train_obs, l_train)
+    y_val_dataset = torch.utils.data.TensorDataset(y_valid_obs, l_valid)
+    y_test_dataset = torch.utils.data.TensorDataset(y_test_obs, l_test)
 
-    y_train_dataset = torch.utils.data.TensorDataset(y_train_obs, label_train)
-    y_val_dataset = torch.utils.data.TensorDataset(y_valid_obs, label_valid)
-    y_test_dataset = torch.utils.data.TensorDataset(y_test_obs, label_test)
-
-    train_dataloader = torch.utils.data.DataLoader(y_train_dataset, batch_size=cfg.batch_sz, shuffle=True)
-    valid_dataloader = torch.utils.data.DataLoader(y_val_dataset, batch_size=y_valid_obs.shape[0], shuffle=False)
-    test_dataloader = torch.utils.data.DataLoader(y_test_dataset, batch_size=y_test_obs.shape[0], shuffle=False)
-
+    train_dataloader = torch.utils.data.DataLoader(
+        y_train_dataset, batch_size=cfg.batch_sz, num_workers=4, pin_memory=True, shuffle=False
+    )
+    valid_dataloader = torch.utils.data.DataLoader(
+        y_val_dataset, batch_size=cfg.batch_sz, num_workers=4, pin_memory=True, shuffle=False
+    )
+    test_dataloader = torch.utils.data.DataLoader(
+        y_test_dataset, batch_size=cfg.batch_sz, num_workers=4, pin_memory=True, shuffle=False
+    ) 
     # Data dimensions
-    n_train_trials, n_time_bins, n_neurons_obs = y_train_obs.shape
+    n_train_trials, n_bins, n_neurons_obs = y_train_obs.shape
     n_valid_trials = y_valid_obs.shape[0]
     n_test_trials = y_test_obs.shape[0]
-    n_time_bins_enc = train_data['n_time_bins_enc']
-
-    batch_sz_train = list(y_train_obs.shape)[:-1]
-    batch_sz_valid = list(y_valid_obs.shape)[:-1]
-    batch_sz_test = list(y_test_obs.shape)[:-1]
-
-    print("# training trials: {0}".format(n_train_trials))
-    print("# validation trials: {0}".format(n_valid_trials))
-    print("# testing trials: {0}".format(n_test_trials))
-    print("# neurons: {0}".format(n_neurons_obs))
-    print("# time bins: {0}".format(n_time_bins))
-    print("# time bins used for forcasting: {0}".format(cfg.n_bins_bhv))
-    print("# predicted time bins: {0}".format(n_time_bins - cfg.n_bins_bhv))
-
+    
+    # Append data-related attributes to the config Dictionary.
+    cfg['n_bins'] = n_bins
+    # Number of time bins used by the model to infere the latents.
+    cfg['n_bins_enc'] = train_data['n_bins_enc']
+    # Number of timesteps used by the model to to predict and unroll the latnt trajectories for n_bins - n_bins_bhv timesteps.
+    cfg['n_bins_bhv'] = cfg.n_bins // 4
+    
+    cfg['n_neurons_obs'] = n_neurons_obs
+    # Number of top most active neurons used by the model to infere the latents.
+    cfg['n_neurons_enc'] = n_neurons_obs
+    
+    cfg = Cfg(cfg)
 
     """likelihood pdf"""
     H = utils.ReadoutLatentMask(cfg.n_latents, cfg.n_latents_read)
@@ -191,19 +183,12 @@ def main():
     ssm = LowRankNonlinearStateSpaceModel(dynamics_mod, likelihood_pdf, initial_condition_pdf, backward_encoder,
                                           local_encoder, nl_filter, device=cfg.device)
 
-    #import multiprocessing as mp
-    #mp.set_start_method('spawn')
-
     seq_vae = LightningMonkeyReaching(ssm, cfg, n_time_bins_enc, cfg.n_bins_bhv)
     csv_logger = CSVLogger('logs/smoother/acausal/', name=f'sd_{cfg.seed}_r_y_{cfg.rank_local}_r_b_{cfg.rank_backward}', version='smoother_acausal')
     ckpt_callback = ModelCheckpoint(save_top_k=3, monitor='r2_valid_enc', mode='max', dirpath='ckpts/smoother/acausal/', save_last=True,
                                     filename='{epoch:0}_{valid_loss:0.2f}_{r2_valid_enc:0.2f}_{r2_valid_bhv:0.2f}_{valid_bps_enc:0.2f}')
 
     trainer = lightning.Trainer(max_epochs=cfg.n_epochs,
-                                #accelerator='gpu',
-                                #devices='auto',
-                                #strategy='deepspeed_stage_1', precision=16,
-                                #strategy='ddp', # raise RuntimeError("Distributed package doesn't have NCCL built in") RuntimeError: Distributed package doesn't have NCCL built in
                                 gradient_clip_val=1.0,
                                 default_root_dir='lightning/',
                                 callbacks=[ckpt_callback],
